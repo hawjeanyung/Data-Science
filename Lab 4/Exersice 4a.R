520f1d88217f49cc772afded2834908b65ca3ca1
Bryan <hawjeanyung@gmail.com>
2026-02-09 10:40:10 +0800

    init

---
+++ Lab 4/Exersice 4a.R
@@ -0,0 +1,9 @@
+data <- c("East","West","East","North","North","East","West","West","West","
+East","North")
+print(data)
+print(is.factor(data))
+
+factor_data <- factor(data)
+print(factor_data)
+print(is.factor(factor_data))
+length(factor_data)
