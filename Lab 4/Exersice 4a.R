982711495eb939fb3f6cbc6589b903cc0fd8ebe5
Bryan <hawjeanyung@gmail.com>
2026-02-09 10:49:23 +0800

    init

--- Lab 4/Exersice 4a.R
+++ Lab 4/Exersice 4a.R
@@ -1,3 +1,4 @@
+#1.1 Factor Construction
 data <- c("East","West","East","North","North","East","West","West","West","
 East","North")
 print(data)
@@ -7,3 +8,9 @@ factor_data <- factor(data)
 print(factor_data)
 print(is.factor(factor_data))
 length(factor_data)
+
+#1.2 Accessing Factors Elements
+data <-
+  factor(c("East","West","East","North","North","East","West","West","
+West","East","North"))
+data[3]
